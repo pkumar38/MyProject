026f4818a6ef8611486df9bf133e9d4bf886bdcb
Pradeep <pkumar38@gmail.com>
2020-02-16 16:03:29 +0800

    Added Frontend Search

--- MyProject/mysite/inv/views.py
+++ MyProject/mysite/inv/views.py
@@ -1,6 +1,16 @@
+import requests
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+def search(request):
+    srch = request.POST.get('txtsearch')
+    context_search = {
+        'base_search':srch,
+    }
+    return render(request, 'inv/search.html',context_search)
